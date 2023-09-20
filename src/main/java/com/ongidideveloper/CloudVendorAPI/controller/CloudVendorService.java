f0055ab720811ce5b6ec45a157da7045d026df1f
ONGIDI E. GEOFREY <ongidigeofrey@gmail.com>
2023-09-20 14:43:40 -0700

    Fix: Add GET AND CREATE functionalities

--- src/main/java/com/ongidideveloper/CloudVendorAPI/controller/CloudVendorService.java
+++ src/main/java/com/ongidideveloper/CloudVendorAPI/controller/CloudVendorService.java
@@ -1,19 +1,22 @@
 package com.ongidideveloper.CloudVendorAPI.controller;
 
 import com.ongidideveloper.CloudVendorAPI.model.CloudVendor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/cloudVendor")
 public class CloudVendorService {
 
+    CloudVendor cloudVendor;
    @PostMapping("{vendorId}")
     public CloudVendor getCloudVendorDetails(String vendorId){
-                return new CloudVendor("C1","CVX","Address 1","254748974489");
-
+                return cloudVendor;
+//                        new CloudVendor("C1","CVX","Address 1","254748974489");
+    }
+    @PostMapping("create")
+    public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor){
+       this.cloudVendor=cloudVendor;
+       return "Cloud Vendor created successfully";
     }
 
 }
